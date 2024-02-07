f5bd0b7b63e464eb43b9b430f0f2b17679522ee7
hands-on-01 <154578446+hands-on-01@users.noreply.github.com>
2024-02-07 21:20:20 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/training_20240207/network/azurerm"
   version = "5.3.0"
   # insert required variables here
-　resource_group_name = azurerm_resource_group.myresourcegroup.name
+　resource_group_name  = azurerm_resource_group.myresourcegroup.name
 }
