5311d3197b0c890baeba5bbfeea2037e4061c578
hands-on-01 <154578446+hands-on-01@users.noreply.github.com>
2024-02-07 21:26:30 +0900

    Delete network.tf

--- network.tf
+++
@@ -1,6 +0,0 @@
-module "network" {
-  source  = "app.terraform.io/training_20240207/network/azurerm"
-  version = "5.3.0"
-  # insert required variables here
-　resource_group_name  = azurerm_resource_group.myresourcegroup.name
-}
